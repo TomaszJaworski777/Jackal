cb8bd00ae8bfa347f433b3b900970068bdb64b45
TomaszJaworski777 <zir3xx@gmail.com>
2024-09-20 19:58:22 +0200

    New value network
    Results of Jackal_dev vs Jackal_base (4+0.04, 1t, MB, Pohl.pgn):
    Elo: 189.28 +/- 22.11, nElo: 291.51 +/- 28.08
    LOS: 100.00 %, DrawRatio: 23.13 %, PairsRatio: 19.55
    Games: 588, Wins: 323, Losses: 31, Draws: 234, Points: 440.0 (74.83 %)
    Ptnml(0-2): [1, 10, 68, 126, 89]
    LLR: 2.95 (-2.94, 2.94) [0.00, 5.00]

--- train/src/value/value_trainer.rs
+++ train/src/value/value_trainer.rs
@@ -14,18 +14,18 @@ impl ValueTrainer {
         .build();
 
         let schedule = TrainingSchedule {
-            net_id: "value_001".to_string(),
+            net_id: "value_002".to_string(),
             eval_scale: 400.0,
             ft_regularisation: 0.0,
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: 80,
+            end_superbatch: 40,
             wdl_scheduler: wdl::ConstantWDL { value: 1.0 },
             lr_scheduler: lr::StepLR {
                 start: 0.001,
                 gamma: 0.1,
-                step: 30,
+                step: 15,
             },
             loss_function: Loss::SigmoidMSE,
             save_rate: 5,
