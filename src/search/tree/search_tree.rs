05ad59fd0c33021fdd5b2a9be6911349a6711e57
TomaszJaworski777 <zir3xx@gmail.com>
2024-09-29 22:28:20 +0200

    Update search_tree.rs

--- src/search/tree/search_tree.rs
+++ src/search/tree/search_tree.rs
@@ -12,7 +12,7 @@ use super::{
 };
 use spear::{ChessBoard, ChessPosition, Move, Side};
 
-const SEGMENT_COUNT: usize = 4;
+const SEGMENT_COUNT: usize = 2;
 
 pub struct SearchTree {
     segments: [TreeSegment; SEGMENT_COUNT],
@@ -25,12 +25,10 @@ impl SearchTree {
         let bytes = size_in_mb * 1024 * 1024;
         let tree_size =
             bytes as usize / (std::mem::size_of::<Node>() + 8 * std::mem::size_of::<Edge>());
-        let segment_size = (tree_size / SEGMENT_COUNT).min(0x3FFFFFFE);
+        let segment_size = (tree_size / SEGMENT_COUNT).min(0x7FFFFFFE);
         let segments = [
             TreeSegment::new(segment_size, 0),
             TreeSegment::new(segment_size, 1),
-            TreeSegment::new(segment_size, 2),
-            TreeSegment::new(segment_size, 3),
         ];
 
         let tree = Self {
