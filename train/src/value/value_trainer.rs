b468bfcc5807155b06253deb9252d0e534ec6b0e
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2024-10-19 01:32:19 +0200

    Threats & Defences (#23)
    Score of Jackal_dev vs Jackal_base: 212 - 51 - 51  [0.756] 314
    ...      Jackal_dev playing White: 115 - 15 - 26  [0.821] 156
    ...      Jackal_dev playing Black: 97 - 36 - 25  [0.693] 158
    ...      White vs Black: 151 - 112 - 51  [0.562] 314
    Elo difference: 196.8 +/- 39.8, LOS: 100.0 %, DrawRatio: 16.2 %
    SPRT: llr 2.94 (100.0%), lbound -2.94, ubound 2.94 - H1 was accepted

--- train/src/value/value_trainer.rs
+++ train/src/value/value_trainer.rs
@@ -1,7 +1,7 @@
 use bullet::{
-    inputs, loader, lr, optimiser, outputs, wdl, LocalSettings, Loss, TrainerBuilder,
-    TrainingSchedule, TrainingSteps,
+    format::{chess::BoardIter, ChessBoard}, inputs, loader, lr, optimiser, outputs, wdl, LocalSettings, Loss, TrainerBuilder, TrainingSchedule, TrainingSteps
 };
+use spear::{Bitboard, Piece, Square};
 
 pub struct ValueTrainer;
 impl ValueTrainer {
@@ -10,7 +10,7 @@ impl ValueTrainer {
             .optimiser(optimiser::AdamW)
             .single_perspective()
             .loss_fn(Loss::SigmoidMSE)
-            .input(inputs::ChessBucketsMirrored::default())
+            .input(ThreatsDefencesMirroredInputs)
             .output_buckets(outputs::Single)
             .feature_transformer(64)
             .activate(bullet::Activation::SCReLU)
@@ -18,7 +18,7 @@ impl ValueTrainer {
             .build();
 
         let schedule = TrainingSchedule {
-            net_id: "value_009a".to_string(),
+            net_id: "value_009td".to_string(),
             eval_scale: 400.0,
             steps: TrainingSteps {
                 batch_size: 16_384,
@@ -46,5 +46,89 @@ impl ValueTrainer {
 
         //trainer.load_from_checkpoint("checkpoints/value_008-80");
         trainer.run(&schedule, &settings, &data_loader);
+    }
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct ThreatsDefencesMirroredInputs;
+
+pub struct ThreatsDefencesMirroredInputsIter {
+    board_iter: BoardIter,
+    threats: Bitboard,
+    defences: Bitboard,
+    flip: usize,
+}
+
+impl inputs::InputType for ThreatsDefencesMirroredInputs {
+    type RequiredDataType = ChessBoard;
+    type FeatureIter = ThreatsDefencesMirroredInputsIter;
+
+    fn buckets(&self) -> usize {
+        1
+    }
+
+    fn max_active_inputs(&self) -> usize {
+        32
+    }
+
+    fn inputs(&self) -> usize {
+        768 * 4
+    }
+
+    fn feature_iter(&self, position: &Self::RequiredDataType) -> Self::FeatureIter {
+        let mut bb = [Bitboard::EMPTY; 8];
+
+        for (pc, sq) in position.into_iter() {
+            let square = Square::from_raw(sq);
+            bb[usize::from(pc >> 3)].set_bit(square);
+            bb[usize::from(2 + (pc & 7))].set_bit(square);
+        }
+
+        let mut board = spear::ChessBoard::default();
+        for idx in 2..8 {
+            let piece = Piece::from_raw(idx as u8 - 2);
+            for side in 0..=1 {
+                (bb[idx] & bb[side]).map(|square| {
+                    if side == 0 {
+                        board.set_piece_on_square::<true>(square, piece);
+                    } else {
+                        board.set_piece_on_square::<false>(square, piece);
+                    }
+                });
+            }
+        }
+
+        let threats = board.generate_attack_map::<true, false>();
+        let defences = board.generate_attack_map::<false, true>();
+
+        ThreatsDefencesMirroredInputsIter {
+            board_iter: position.into_iter(),
+            threats,
+            defences,
+            flip: if position.our_ksq() % 8 > 3 { 7 } else { 0 },
+        }
+    }
+}
+
+impl Iterator for ThreatsDefencesMirroredInputsIter {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.board_iter.next().map(|(piece, square)| {
+            let piece_index = usize::from(piece & 7);
+            let square = usize::from(square);
+            let side = usize::from(piece & 8 > 0);
+            let mut input = (side * 384) + (64 * piece_index) + (square ^ self.flip);
+
+            if self.threats.get_bit(Square::from_raw(square as u8)) {
+                input += 768;
+            }
+
+            if self.defences.get_bit(Square::from_raw(square as u8)) {
+                input += 768 * 2;
+            }
+
+            (input, input)
+        })
     }
 }
