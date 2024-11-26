5cced7430467221cf2c2d2b49bc53d742f65a68c
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2024-11-26 22:16:46 +0100

    Adjust hash table size (#42)

--- src/processors/uci_processor.rs
+++ src/processors/uci_processor.rs
@@ -40,7 +40,7 @@ impl UciProcessor {
 
         if command == "Hash" {
             let hash_size = search_engine.engine_options().hash();
-            let hash_percentage = search_engine.engine_options().hash_percentage();
+            let hash_percentage = search_engine.engine_options().hash_percentage() / 10.0;
             search_engine.tree_mut().resize_tree(hash_size, hash_percentage)
         }
     }
