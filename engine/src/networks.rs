0724813383e6e7415851c190e654f76f696d5926
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-12 02:40:42 +0100

    4.05b positions
    Bench: 2923636

--- engine/src/networks.rs
+++ engine/src/networks.rs
@@ -13,5 +13,5 @@ pub static ValueNetwork: ValueNetwork = unsafe {
 
 #[allow(non_upper_case_globals)]
 pub static PolicyNetwork: PolicyNetwork = unsafe {
-    std::mem::transmute(*include_bytes!("../../resources/networks/test_400.network"))
+    std::mem::transmute(*include_bytes!("../../resources/networks/p200exp2048pwsee006qb.network"))
 };
