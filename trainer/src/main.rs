17a9a644bc130076b73b1c3649e9f5f70eb302c6
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-13 12:45:10 +0100

    Remove trainer
    Bench: 2878454

--- trainer/src/main.rs
+++ trainer/src/main.rs
@@ -1,19 +1,5 @@
-mod policy_train;
-mod value_train;
-mod interleave;
+mod convert;
 
 fn main() {
-    #[cfg(feature = "policy_trainer")] {
-        policy_train::run();
-    }
-
-    #[cfg(feature = "value_trainer")] {
-        value_train::run();
-    }
-
-    #[cfg(feature = "policy_interleave")] {
-        use crate::interleave::interleave;
-
-        _ = interleave();
-    }
+    convert::conv();
 }
