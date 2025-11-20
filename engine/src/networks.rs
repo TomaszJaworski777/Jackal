748a812f692eb8d8bd8a2745f06a0c252a6155d4
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-20 18:34:41 +0100

    Base net
    Bench: 3229928

--- engine/src/networks.rs
+++ engine/src/networks.rs
@@ -8,7 +8,7 @@ use crate::networks::policy_network::PolicyNetwork;
 
 #[allow(non_upper_case_globals)]
 pub static ValueNetwork: ValueNetwork = unsafe {
-    std::mem::transmute(*include_bytes!("../../resources/networks/v800exp2048wdl008qa.network"))
+    std::mem::transmute(*include_bytes!("../../resources/networks/test_1_3072.network"))
 };
 
 #[allow(non_upper_case_globals)]
