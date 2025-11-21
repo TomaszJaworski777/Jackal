7608499ed0d77648d7667410c43d6765d070d356
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-21 17:17:20 +0100

    B - A
    Bench: 3223934

--- engine/src/networks/inputs/threads_extended.rs
+++ engine/src/networks/inputs/threads_extended.rs
@@ -1,13 +1,12 @@
-use chess::{Bitboard, ChessBoard, Piece, Rays, Side};
+use chess::{Bitboard, Piece, Rays, Side};
 
-const BASE_INPUTS: usize = 768 * 7 * 7;
 const STATE_INPUTS: usize = 768 * 6;
 
 pub struct ThreatsExtended;
 #[allow(unused)]
 impl ThreatsExtended {
     pub const fn input_size() -> usize {
-        BASE_INPUTS + STATE_INPUTS
+        STATE_INPUTS
     }
 
     pub fn map_inputs<F: FnMut(usize)>(board: &chess::ChessBoard, mut process_input: F) {
@@ -36,58 +35,20 @@ impl ThreatsExtended {
             let attackers = all_attackers & board.occupancy_for_side(board.side().flipped()) & !attack_pin_mask;
             let defenders = all_attackers & board.occupancy_for_side(board.side()) & !defender_pin_mask;
 
-            let (attacker, defender) = attacker_defender(&board, attackers, defenders);
-
             let piece_index = 64 * (u8::from(piece) - u8::from(Piece::PAWN)) as usize;
             let base = [384, 0][usize::from(color == board.side())] + piece_index + (usize::from(square) ^ horizontal_mirror ^ if flip { 56 } else { 0 });
-            let mut feat = base;
 
-            if attacker != Piece::NONE {
-                feat += 768 * (usize::from(attacker) + 1)
-            }
-
-            if defender != Piece::NONE {
-                feat += 768 * 7 * (usize::from(defender) + 1)
-            }
-
-            process_input(feat);
-
-            let feat = BASE_INPUTS + 768 * calculate_state(piece, attacker, defender, attackers, defenders) + base;
+            let feat = 768 * calculate_state(board, piece, attackers, defenders) + base;
 
             process_input(feat)
         });
     }
 }
 
-fn attacker_defender(board: &ChessBoard, attackers: Bitboard, defenders: Bitboard) -> (Piece, Piece) {
-    let attacker = lowest_value_piece(board, attackers);
-    let defender = lowest_value_piece(board, defenders);
-    (attacker, defender)
-}
+fn calculate_state(board: &chess::ChessBoard, victim: Piece, attackers: Bitboard, defenders: Bitboard) -> usize {
+    let lowest_attacker = lowest_value_piece(board, attackers);
+    let lowest_defender = lowest_value_piece(board, defenders);
 
-fn lowest_value_piece(board: &ChessBoard, mask: Bitboard) -> Piece {
-    if mask.is_empty() {
-        return Piece::NONE;
-    }
-
-    if (mask & board.piece_mask(Piece::PAWN)).is_not_empty() {
-        Piece::PAWN
-    } else if (mask & board.piece_mask(Piece::KNIGHT)).is_not_empty() {
-        Piece::KNIGHT
-    } else if (mask & board.piece_mask(Piece::BISHOP)).is_not_empty() {
-        Piece::BISHOP
-    } else if (mask & board.piece_mask(Piece::ROOK)).is_not_empty() {
-        Piece::ROOK
-    } else if (mask & board.piece_mask(Piece::QUEEN)).is_not_empty() {
-        Piece::QUEEN
-    } else if (mask & board.piece_mask(Piece::KING)).is_not_empty() {
-        Piece::KING
-    } else {
-        Piece::NONE
-    }
-}
-
-fn calculate_state(victim: Piece, lowest_attacker: Piece, lowest_defender: Piece, attackers: Bitboard, defenders: Bitboard) -> usize {
     let atk_cnt = attackers.pop_count();
     let def_cnt = defenders.pop_count();
 
@@ -136,5 +97,27 @@ fn calculate_state(victim: Piece, lowest_attacker: Piece, lowest_defender: Piece
         return 2;
     } else {
         return 3
+    }
+}
+
+fn lowest_value_piece(board: &chess::ChessBoard, mask: Bitboard) -> Piece {
+    if mask.is_empty() {
+        return Piece::NONE;
+    }
+
+    if (mask & board.piece_mask(Piece::PAWN)).is_not_empty() {
+        Piece::PAWN
+    } else if (mask & board.piece_mask(Piece::KNIGHT)).is_not_empty() {
+        Piece::KNIGHT
+    } else if (mask & board.piece_mask(Piece::BISHOP)).is_not_empty() {
+        Piece::BISHOP
+    } else if (mask & board.piece_mask(Piece::ROOK)).is_not_empty() {
+        Piece::ROOK
+    } else if (mask & board.piece_mask(Piece::QUEEN)).is_not_empty() {
+        Piece::QUEEN
+    } else if (mask & board.piece_mask(Piece::KING)).is_not_empty() {
+        Piece::KING
+    } else {
+        Piece::NONE
     }
 }
