620f4efa5838d08722f924833dace72382217d1a
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-21 23:33:12 +0100

    Stage C
    Bench: 3284864

--- engine/src/networks.rs
+++ engine/src/networks.rs
@@ -8,7 +8,7 @@ use crate::networks::policy_network::PolicyNetwork;
 
 #[allow(non_upper_case_globals)]
 pub static ValueNetwork: ValueNetwork = unsafe {
-    std::mem::transmute(*include_bytes!("../../resources/networks/stageB-A.network"))
+    std::mem::transmute(*include_bytes!("../../resources/networks/stageC.network"))
 };
 
 #[allow(non_upper_case_globals)]
